f1603c25943a8be33982c6bccbf4c59b7adffcdf
Brandon Cordell <brandon@brandoncordell.com>
2024-09-11 20:11:59 -0400

    Quiet sass deprecations

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,19 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true
+      }
+    }
+  },
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
